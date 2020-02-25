7246ed9c5c0fbd7b83ca618a29b8d6544d9d5051
eliasvolkov <46112221+eliasvolkov@users.noreply.github.com>
2020-02-26 00:00:19 +0300

    add adaptive for header (#7)

--- src/organisms/Header/Header.styles.ts
+++ src/organisms/Header/Header.styles.ts
@@ -1,6 +1,11 @@
 import { COLORS } from 'constants/ui';
 import styled from 'styled-components';
+import { Close } from 'styled-icons/remix-fill/Close';
 import { Menu2 } from 'styled-icons/remix-fill/Menu2';
+
+interface INavWrapper {
+    isOpen: boolean;
+}
 
 export const Wrapper = styled.div`
     display: flex;
@@ -13,12 +18,20 @@ export const Wrapper = styled.div`
 export const LogoWrapper = styled.div`
     width: 9rem;
 `;
-export const NavWrapper = styled.div`
+
+export const NavWrapper = styled.div<INavWrapper>`
     margin-left: auto;
     margin-right: 5rem;
 
-    @media screen and (max-width: 800px) {
-        display: none;
+    @media screen and (max-width: 995px) {
+        transition: all 0.5s;
+        width: 50%;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        right: 0;
+        margin-right: ${({ isOpen }) => (isOpen ? '0' : '-50%')};
+        background-color: ${COLORS.background};
     }
 `;
 
@@ -26,8 +39,22 @@ export const MenuIcon = styled(Menu2)`
     color: ${COLORS.primaryText};
     display: none;
     margin-left: auto;
+    cursor: pointer;
 
-    @media screen and (max-width: 800px) {
+    @media screen and (max-width: 995px) {
+        display: block;
+    }
+`;
+
+export const CloseIcon = styled(Close)`
+    position: absolute;
+    top: 0;
+    right: 0;
+    color: ${COLORS.primaryText};
+    display: none;
+    cursor: pointer;
+
+    @media screen and (max-width: 995px) {
         display: block;
     }
 `;
