d581215052ffbe5c12df9402ae5e95085becf3bf
eliasvolkov <ilya.j0b@yandex.ru>
2020-03-01 00:51:57 +0300

    [#1] implement test store

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
+import { configure } from 'mobx';
+import { Provider } from 'mobx-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moviesStore from 'stores/moviesStore';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+configure({ enforceActions: 'observed' });
+
+ReactDOM.render(
+    <Provider moviesStore={moviesStore}>
+        <App />
+    </Provider>,
+
+    document.getElementById('root'),
+);
 serviceWorker.unregister();
